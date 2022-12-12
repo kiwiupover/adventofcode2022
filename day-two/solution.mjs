b85509c4fd31a33b2aed488a2d0591825a3882f0
Dave Laird <kiwiupover@gmail.com>
2022-12-12 07:57:52 -0800

    Day two done

--- day-two/solution.mjs
+++ day-two/solution.mjs
@@ -1,5 +1,14 @@
 import { readFile } from "fs/promises";
 
+/*
+  A = Rock > Z
+  B = Paper > X
+  C = Scissors > Y
+  X = Rock > C
+  Y = Paper > A
+  Z = Scissors > B
+*/
+
 let playerOne = 0;
 let playerTwo = 0;
 let p1 = 0;
@@ -53,13 +62,46 @@ const scoreForPlayedType = (input) => {
   return 0;
 };
 
-games.forEach((game) => {
-  const { p1, p2 } = rockPaperScissorsWinner(game[0], game[2]);
-  const one = scoreForPlayedType(game[0]);
-  const two = scoreForPlayedType(game[2]);
+const toWinIMustPlay = (input1, input2) => {
+  if (
+    (input1 === "A" && input2 === "Y") ||
+    (input1 === "B" && input2 === "X") ||
+    (input1 === "C" && input2 === "Z")
+  ) {
+    return "X";
+  }
 
+  if (
+    (input1 === "A" && input2 === "Z") ||
+    (input1 === "B" && input2 === "Y") ||
+    (input1 === "C" && input2 === "X")
+  ) {
+    return "Y";
+  }
+
+  if (
+    (input1 === "A" && input2 === "X") ||
+    (input1 === "B" && input2 === "Z") ||
+    (input1 === "C" && input2 === "Y")
+  ) {
+    return "Z";
+  }
+};
+
+games.forEach((game) => {
+  console.log({ game });
+  const { p1, p2 } = rockPaperScissorsWinner(
+    game[0],
+    toWinIMustPlay(game[0], game[2])
+  );
+  const one = scoreForPlayedType(game[0]);
+  const two = scoreForPlayedType(toWinIMustPlay(game[0], game[2]));
+
+  console.log({ p1, one, p2, two });
   const pp1 = p1 + one;
   const pp2 = p2 + two;
+
+  console.log({ pp1, pp2 });
 
   playerOne = playerOne + pp1;
   playerTwo = playerTwo + pp2;
